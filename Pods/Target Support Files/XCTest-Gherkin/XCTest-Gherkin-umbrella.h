65e18c82ee4970cc14f8c3995b5d1eb771b48b02
zvalentine <zvalentine@pillartechnology.com>
2019-05-23 13:20:50 -0500

    add gem files and pods

---
+++ Pods/Target Support Files/XCTest-Gherkin/XCTest-Gherkin-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UnusedStepsTracker.h"
+
+FOUNDATION_EXPORT double XCTest_GherkinVersionNumber;
+FOUNDATION_EXPORT const unsigned char XCTest_GherkinVersionString[];
+
