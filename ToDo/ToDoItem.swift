71dcd76323e643a18423ab53f896332bf6377da9
Aaron Harris <aamharris@gmail.com>
2019-05-30 09:42:11 -0500

    update to do item to match service now

--- ToDo/ToDoItem.swift
+++ ToDo/ToDoItem.swift
@@ -1,13 +1,20 @@
 import UIKit
 
 public class ToDoItem : Codable {
+    var _id: String = ""
     var title: String = ""
     var body: String = ""
-    var status: Bool = false
+    var isComplete: Bool = false
     
     init(title: String, body: String) {
         self.title = title
         self.body = body
-        self.status = false
+    }
+    
+    init(id: String, title: String, body: String, isComplete: Bool) {
+        self._id = id
+        self.title = title
+        self.body = body
+        self.isComplete = isComplete
     }
 }
